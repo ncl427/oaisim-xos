223f0f9005161b3b3792a75a9224f61a9d9be2f5
javier <shaifvier@gmail.com>
2019-01-02 23:41:14 -0800

    Changed names of files. Replaced legacy VBBU naming to OAISIM names for the service files

--- xos/synchronizer/oaisim-synchronizer.py
+++ xos/synchronizer/oaisim-synchronizer.py
@@ -14,4 +14,21 @@
 # limitations under the License.
 
 
-python oaisim-synchronizer.py
+#!/usr/bin/env python
+
+# Runs the standard XOS synchronizer
+
+import importlib
+import os
+import sys
+from xosconfig import Config
+
+config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/oaisim_config.yaml')
+Config.init(config_file, 'synchronizer-config-schema.yaml')
+
+synchronizer_path = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), "../../synchronizers/new_base")
+sys.path.append(synchronizer_path)
+mod = importlib.import_module("xos-synchronizer")
+mod.main()
+
